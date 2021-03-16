3648a18f9dede89ae5556c0a2136abf3c17a7e04
rht48 <romain.hubert48@yahoo.com>
2021-03-16 17:36:50 +0100

    Tweaks in game and comments

--- src/main/java/model/game/Game.java
+++ src/main/java/model/game/Game.java
@@ -1,28 +1,46 @@
 package model.game;
 
 
+import graphics.TexturedModel;
 import model.entity.Bird;
 import model.entity.Entity;
 import model.entity.Pipe;
+import processing.core.PVector;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
 
-    private Bird bird;
-    private Entity pipe;
-    private final List<Entity> pipes;
+    /* Textured Models for spawning entities */
+    private final TexturedModel birdModel;
+    private final TexturedModel pipeModel;
 
-    public Game(final Bird bird) {
-        this.bird = bird;
+    /* Entities for the game */
+    private final Bird bird;
+    private final List<Pipe> pipes;
+
+    public Game(final TexturedModel birdModel, final TexturedModel pipeModel) {
+        this.birdModel = birdModel;
+        this.pipeModel = pipeModel;
+
+        this.bird = new Bird(this.birdModel,
+                new PVector(100, 100),
+                0, 0, 0,
+                0.1f, 0.1f, 0);
         this.pipes = new ArrayList<>();
     }
 
+    /**
+     * Updates the game
+     */
     public void update() {
         this.bird.update();
     }
 
+    /**
+     * Makes the bird jump
+     */
     public void makeBirdJump() {
         this.bird.jump();
     }
@@ -31,7 +49,7 @@ public class Game {
         return this.bird;
     }
 
-    public List<Entity> getPipes() {
+    public List<Pipe> getPipes() {
         return this.pipes;
     }
 }
