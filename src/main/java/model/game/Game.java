33bd692976c18050cc96bfb7195c9ccff875abaf
rht48 <romain.hubert48@yahoo.com>
2021-03-16 17:06:27 +0100

    Add Bird and Pipe textures

--- src/main/java/model/game/Game.java
+++ src/main/java/model/game/Game.java
@@ -1,4 +1,28 @@
 package model.game;
 
+import model.entity.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+
+    private Entity bird;
+    private final List<Entity> pipes;
+
+    public Game() {
+        this.pipes = new ArrayList<>();
+    }
+
+    public void update() {
+
+    }
+
+    public Entity getBird() {
+        return this.bird;
+    }
+
+    public List<Entity> getPipes() {
+        return this.pipes;
+    }
 }
